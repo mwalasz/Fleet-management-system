07b5a48c3f6b8ad6396453a7f26914ea08c54018
mwalasz <mwalasz@interia.pl>
2020-10-07 23:05:27 +0200

    Dodanie DTO dla konta użytkownika oraz konta menedżera

---
+++ srv/src/FleetManagement/Entities/ManagerAccounts/DTO/ManagerAccountDto.cs
@@ -0,0 +1,9 @@
+﻿using FleetManagement.Entities.UserAccounts.DTO;
+
+namespace FleetManagement.Entities.ManagerAccounts.DTO
+{
+    public class ManagerAccountDto
+    {
+        public UserAccountDto Account { get; set; }
+    }
+}
---
+++ srv/src/FleetManagement/Entities/UserAccounts/DTO/UserAccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FleetManagement.Entities.UserAccounts.DTO
+{
+    public class UserAccountDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
