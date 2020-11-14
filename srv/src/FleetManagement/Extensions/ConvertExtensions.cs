ef269bc8b49d90b9aec58675dd724af2bfe3b831
mwalasz <mwalasz@interia.pl>
2020-11-14 18:21:40 +0100

    Added class and package for unit conversions

---
+++ srv/src/FleetManagement/Extensions/ConvertExtensions.cs
@@ -0,0 +1,33 @@
+﻿using UnitsNet;
+using UnitsNet.Units;
+
+namespace FleetManagement.Extensions
+{
+    public static class ConvertExtensions
+    {
+        public static double FromMetersToKilometers(this double meters)
+        {
+            return Length.FromMeters(meters).ToUnit(LengthUnit.Kilometer).Value;
+        }
+
+        public static double FromMetersPerSecondToKilometersPerHour(this double metersPerSeconds)
+        {
+            return Speed.FromMetersPerSecond(metersPerSeconds).ToUnit(SpeedUnit.KilometerPerHour).Value;
+        }
+
+        public static double FromKilometersPerHourToMetersPerSecond(this double kilometersPerHour)
+        {
+            return Speed.FromKilometersPerHour(kilometersPerHour).ToUnit(SpeedUnit.MeterPerSecond).Value;
+        }
+
+        public static double FromSecondsToMinutes(this double seconds)
+        {
+            return Duration.FromSeconds(seconds).ToUnit(DurationUnit.Minute).Value;
+        }
+
+        public static double FromSecondsToHours(this double seconds)
+        {
+            return Duration.FromSeconds(seconds).ToUnit(DurationUnit.Hour).Value;
+        }
+    }
+}
