811a62d6eab6b39c1162886ec7567e61d618ceed
mwalasz <mwalasz@interia.pl>
2020-11-14 19:50:43 +0100

    Splitted users controller to two separate ones

---
+++ srv/src/FleetManagement/Controllers/DriversController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using FleetManagement.Entities.Accounts.DriverAccounts;
+using FleetManagement.Entities.Accounts.DriverAccounts.DTO;
+using FleetManagement.Entities.Accounts.DriverAccounts.Models;
+using FleetManagement.Entities.Accounts.DriverAccounts.Params;
+using FleetManagement.Entities.Companies.Models;
+using FleetManagement.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FleetManagement.Controllers
+{
+    [ApiController]
+    [DefaultRoute]
+    [AllowAnonymous]
+    //[Authorize(Roles = CustomRoles.AdminAndManager)]
+    public class DriversController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IDriverAccountProvider driverAccountProvider;
+
+        public DriversController(IMapper mapper, IDriverAccountProvider driverAccountProvider)
+        {
+            this.mapper = mapper;
+            this.driverAccountProvider = driverAccountProvider;
+        }
+
+        [HttpGet]
+        public IEnumerable<DriverAccountDto> GetAll()
+        {
+            return driverAccountProvider.GetAll()
+                .Select(driver => mapper.Map<DriverAccount, DriverAccountDto>(driver));
+        }
+
+        [HttpGet]
+        public IActionResult GetDriverCompany(string mail)
+        {
+            var company = driverAccountProvider.GetDriverCompany(mail);
+
+            if (company != null)
+                return Ok(mapper.Map<Company, CompanyDto>(company));
+            else return Ok(company);
+        }
+
+        [HttpPost]
+        public IActionResult AddNewDriver([FromQuery] NewDriverAccountParams newDriverParams)
+        {
+            var newDriverId = driverAccountProvider.AddNewAndGetId(newDriverParams);
+            
+            if (newDriverId != -1)
+            {
+                var newDriver = driverAccountProvider.GetById(newDriverId);
+
+                var toReturn = (newDriver != null)
+                    ? mapper.Map<DriverAccount, DriverAccountDto>(newDriver)
+                    : null;
+
+                return Ok(toReturn);
+            }
+
+            return Ok("Błąd w trakcie dodawania nowego kierowcy!");
+        }
+    }
+}
---
+++ srv/src/FleetManagement/Controllers/ManagersController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using FleetManagement.Entities.Accounts.ManagerAccounts;
+using FleetManagement.Entities.Accounts.ManagerAccounts.DTO;
+using FleetManagement.Entities.Accounts.ManagerAccounts.Models;
+using FleetManagement.Entities.Accounts.ManagerAccounts.Params;
+using FleetManagement.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FleetManagement.Controllers
+{
+    [ApiController]
+    [DefaultRoute]
+    [AllowAnonymous]
+    public class ManagersController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IManagerAccountProvider managerAccountProvider;
+
+        public ManagersController(IMapper mapper,
+            IManagerAccountProvider managerAccountProvider)
+        {
+            this.mapper = mapper;
+            this.managerAccountProvider = managerAccountProvider;
+        }
+
+        [HttpGet]
+        public IEnumerable<ManagerAccountDto> GetAll()
+        {
+            return managerAccountProvider.GetAll()
+                .Select(manager => mapper.Map<ManagerAccount, ManagerAccountDto>(manager));
+        }
+
+        [HttpPost]
+        public IActionResult AddNewManager([FromQuery] NewManagerAccountParams newDriverParams)
+        {
+            var newManagerId = managerAccountProvider.AddNewAndGetId(newDriverParams);
+
+            if (newManagerId != -1)
+            {
+                var newManager = managerAccountProvider.GetById(newManagerId);
+
+                var toReturn = (newManager != null)
+                    ? mapper.Map<ManagerAccount, ManagerAccountDto>(newManager)
+                    : null;
+
+                return Ok(toReturn);
+            }
+
+            return Ok("Błąd w trakcie dodawania nowego menadżera!");
+        }
+    }
+}
