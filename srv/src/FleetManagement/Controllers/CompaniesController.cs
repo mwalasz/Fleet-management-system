9064e2ec9499688be2ae10d2603a3e1fe6945738
mwalasz <mwalasz@interia.pl>
2020-11-27 19:49:38 +0100

    Added endpoint for adding new companies

--- srv/src/FleetManagement/Controllers/CompaniesController.cs
+++ srv/src/FleetManagement/Controllers/CompaniesController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using FleetManagement.Entities.Accounts.ManagerAccounts;
 using FleetManagement.Entities.Companies;
 using FleetManagement.Entities.Companies.Models;
+using FleetManagement.Entities.Companies.Params;
 using FleetManagement.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,11 +17,13 @@ namespace FleetManagement.Db.Repositories
     {
         private readonly ICompanyProvider companyProvider;
         private readonly IMapper mapper;
+        private readonly IManagerAccountProvider managerAccountProvider;
 
-        public CompaniesController(ICompanyProvider companyProvider, IMapper mapper)
+        public CompaniesController(ICompanyProvider companyProvider, IMapper mapper, IManagerAccountProvider managerAccountProvider)
         {
             this.companyProvider = companyProvider;
             this.mapper = mapper;
+            this.managerAccountProvider = managerAccountProvider;
         }
 
         [HttpGet]
@@ -27,6 +31,35 @@ namespace FleetManagement.Db.Repositories
         {
             var companies = companyProvider.GetAll();
             return Ok(companies.Select(c => mapper.Map<Company, CompanyDto>(c)));
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] NewCompanyParam newCompanyParam)
+        {
+            var manager = managerAccountProvider.GetByMail(newCompanyParam.ManagerAccountMail);
+
+            if (manager == null)
+                return BadRequest("Nie znaleziono managera o podanym adresie email!");
+
+            try
+            {
+                companyProvider.Add(new Company()
+                {
+                    Address = newCompanyParam.Address,
+                    Name = newCompanyParam.Name,
+                    Description = newCompanyParam.Description,
+                    Mail = newCompanyParam.Mail,
+                    NIP = newCompanyParam.NIP,
+                    PhoneNumber = newCompanyParam.PhoneNumber,
+                    ManagerAccountId = manager.Id,
+                });
+
+                return Ok();
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest($"Error while adding new company: {e.Message}");
+            }
         }
     }
 }
---
+++ srv/src/FleetManagement/Entities/Companies/Params/NewCompanyParam.cs
@@ -0,0 +1,13 @@
+﻿namespace FleetManagement.Entities.Companies.Params
+{
+    public class NewCompanyParam
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string NIP { get; set; }
+        public string Address { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ManagerAccountMail { get; set; }
+    }
+}
